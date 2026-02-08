6a0048cbb5dc525c323c35b7d4e5722a600e2b41
kovardin <artem-kovardin@yandex.ru>
2026-02-09 00:03:11 +0300

    archive groups

--- admin/internal/modules/ads/builders/group.go
+++ admin/internal/modules/ads/builders/group.go
@@ -2,8 +2,12 @@ package builders
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/qor5/admin/v3/presets"
+	"github.com/qor5/admin/v3/presets/gorm2op"
 	"github.com/qor5/web/v3"
 	v "github.com/qor5/x/v3/ui/vuetify"
 	"github.com/qor5/x/v3/ui/vuetifyx"
@@ -28,7 +32,11 @@ func NewGroup(logger *zap.Logger, db *gorm.DB) *Group {
 }
 
 // Константа для имени события копирования
-const copyGroupEvent = "copyGroup"
+const (
+	copyGroupEvent      = "copyGroup"
+	archiveGroupEvent   = "archiveGroup"
+	unarchiveGroupEvent = "unarchiveGroup"
+)
 
 func (m *Group) Configure(b *presets.Builder) {
 	mg := b.Model(&models.Bgroup{}).
@@ -36,7 +44,49 @@ func (m *Group) Configure(b *presets.Builder) {
 		// Label("Группы").
 		RightDrawerWidth("1000")
 
-	mgl := mg.Listing("ID", "Title", "Price", "Start", "End", "Campaign", "Active")
+	mgl := mg.Listing("ID", "Title", "Price", "Start", "End", "Campaign", "Active").
+		SearchFunc(func(ctx *web.EventContext, params *presets.SearchParams) (result *presets.SearchResult, err error) {
+			exist := false
+			for _, v := range params.SQLConditions {
+				if strings.Contains(v.Query, "archived_at is not null") {
+					exist = true
+					break
+				}
+
+				if strings.Contains(v.Query, "(archived_at is not null or archived_at is null)") {
+					exist = true
+					break
+				}
+			}
+
+			if !exist {
+				qdb := m.db.Where("archived_at is null")
+				return gorm2op.DataOperator(qdb).Search(ctx, params)
+			} else {
+				qdb := m.db.Where("")
+				return gorm2op.DataOperator(qdb).Search(ctx, params)
+			}
+		}).
+		SearchColumns("Title").
+		SelectableColumns(true).
+		OrderableFields([]*presets.OrderableField{
+			{
+				FieldName: "ID",
+				DBColumn:  "id",
+			},
+			{
+				FieldName: "Title",
+				DBColumn:  "title",
+			},
+			{
+				FieldName: "Start",
+				DBColumn:  "start",
+			},
+			{
+				FieldName: "End",
+				DBColumn:  "end",
+			},
+		})
 
 	mgl.FilterDataFunc(func(ctx *web.EventContext) vuetifyx.FilterData {
 		// msgr := i18n.MustGetModuleMessages(ctx.R, presets.ModelsI18nModuleKey, Messages_en_US).(*Messages)
@@ -48,11 +98,54 @@ func (m *Group) Configure(b *presets.Builder) {
 
 		return []*vuetifyx.FilterItem{
 			{
+				Key:          "archived",
+				Label:        "Архив",
+				ItemType:     vuetifyx.ItemTypeSelect,
+				SQLCondition: "archived_at is null",
+				Options: []*vuetifyx.SelectItem{
+					{
+
+						Text:         "В архиве",
+						Value:        "is_archived",
+						SQLCondition: "archived_at is not null",
+					},
+					{
+						Text:         "Все",
+						Value:        "all",
+						SQLCondition: "(archived_at is not null or archived_at is null)",
+					},
+				},
+			},
+			{
 				Key:          "campaign",
 				Label:        "Кампания",
 				ItemType:     vuetifyx.ItemTypeSelect,
 				SQLCondition: `campaign_id %s ?`,
 				Options:      options,
+			},
+			{
+				Key:      "active",
+				Label:    "Активность",
+				ItemType: vuetifyx.ItemTypeSelect,
+				Options: []*vuetifyx.SelectItem{
+					{
+
+						Text:         "Включен",
+						Value:        "is_active",
+						SQLCondition: "active = true",
+					},
+					{
+						Text:         "Выключен",
+						Value:        "not_active",
+						SQLCondition: "active = false",
+					},
+				},
+			},
+			{
+				Key:          "created",
+				Label:        "Создан",
+				ItemType:     vuetifyx.ItemTypeDate,
+				SQLCondition: `created_at %s ?`,
 			},
 		}
 	})
@@ -66,17 +159,44 @@ func (m *Group) Configure(b *presets.Builder) {
 
 		m.db.First(&comp, "id = ?", c.CampaignID)
 
-		return h.Td().Text(comp.Title)
+		return h.Td().Children(
+			h.A().
+				Text(comp.Title).
+				Attr("onclick", "event.stopPropagation();").
+				Href(fmt.Sprintf("/admin/bgroups?f_campaign=%d", c.CampaignID)),
+		)
 	})
 
 	mgl.Field("Title").ComponentFunc(func(obj interface{}, field *presets.FieldContext, ctx *web.EventContext) h.HTMLComponent {
 		c := obj.(*models.Bgroup)
+
+		style := ""
+		text := ""
+		if c.ArchivedAt != nil {
+			style = "color:#bb0"
+			text = " - архив"
+		}
+
 		return h.Td().Children(
 			h.A().
-				Text(c.Title).
+				Text(c.Title+text).
+				Style(style).
 				Attr("onclick", "event.stopPropagation();").
 				Href(fmt.Sprintf("/admin/banners?f_group=%d", c.ID)),
 		)
+	})
+
+	mgl.Field("Active").ComponentFunc(func(obj interface{}, field *presets.FieldContext, ctx *web.EventContext) h.HTMLComponent {
+		c := obj.(*models.Bgroup)
+
+		color := "red"
+		text := "выключен"
+		if c.Active {
+			text = "включен"
+			color = "green"
+		}
+
+		return h.Td().Children(h.Span(text).Style("color:" + color))
 	})
 
 	mgl.Field("Price").Label("CPM")
@@ -98,8 +218,38 @@ func (m *Group) Configure(b *presets.Builder) {
 			)
 		})
 
+	mgn.RowMenuItem("Archive").
+		ComponentFunc(func(obj interface{}, id string, ctx *web.EventContext) h.HTMLComponent {
+			item := obj.(*models.Bgroup)
+			if item.ArchivedAt == nil {
+				return v.VListItem(
+					web.Slot(
+						v.VIcon("mdi-archive-arrow-down"), // Используем иконку копирования
+					).Name("prepend"),
+					v.VListItemTitle(
+						h.Text("Архивировать"),
+					),
+				).Attr("@click",
+					web.Plaid().EventFunc(archiveGroupEvent).Query("id", id).Go(),
+				)
+			} else {
+				return v.VListItem(
+					web.Slot(
+						v.VIcon("mdi-archive-arrow-up"), // Используем иконку копирования
+					).Name("prepend"),
+					v.VListItemTitle(
+						h.Text("Разархивировать"),
+					),
+				).Attr("@click",
+					web.Plaid().EventFunc(unarchiveGroupEvent).Query("id", id).Go(),
+				)
+			}
+		})
+
 	// Регистрируем обработчик события копирования
 	mg.RegisterEventFunc(copyGroupEvent, m.copyGroup)
+	mg.RegisterEventFunc(archiveGroupEvent, m.archiveGroup)
+	mg.RegisterEventFunc(unarchiveGroupEvent, m.unarchiveGroup)
 
 	mge := mg.Editing(
 		&presets.FieldsSection{
@@ -185,12 +335,67 @@ func (m *Group) copyGroup(ctx *web.EventContext) (r web.EventResponse, err error
 	}
 
 	// Создаем копию
-	if _, err := original.Copy(m.db, original.CampaignID); err != nil {
-		return r, fmt.Errorf("failed to create copy: %w", err)
+	ng, err := original.Copy(m.db, original.CampaignID)
+	if err != nil {
+		return r, fmt.Errorf("failed on copy group: %w", err)
 	}
 
 	// Обновляем список
-	r.Reload = true
+	r.Emit(
+		presets.NotifModelsUpdated(&models.Bgroup{}),
+		presets.PayloadModelsUpdated{Ids: []string{id, strconv.Itoa(int(ng.ID))}},
+	)
+
+	return r, nil
+}
+
+func (m *Group) archiveGroup(ctx *web.EventContext) (r web.EventResponse, err error) {
+	id := ctx.R.FormValue("id")
+	if id == "" {
+		return r, fmt.Errorf("id is required")
+	}
+
+	// Находим оригинальную запись
+	var original models.Bgroup
+	if err := m.db.First(&original, id).Error; err != nil {
+		return r, fmt.Errorf("failed to find group: %w", err)
+	}
+
+	now := time.Now()
+	if err := original.Archive(m.db, &now); err != nil {
+		return r, fmt.Errorf("failed to archive group: %w", err)
+	}
+
+	// Обновляем список
+	r.Emit(
+		presets.NotifModelsUpdated(&models.Bgroup{}),
+		presets.PayloadModelsUpdated{Ids: []string{id}},
+	)
+
+	return r, nil
+}
+
+func (m *Group) unarchiveGroup(ctx *web.EventContext) (r web.EventResponse, err error) {
+	id := ctx.R.FormValue("id")
+	if id == "" {
+		return r, fmt.Errorf("id is required")
+	}
+
+	// Находим оригинальную запись
+	var original models.Bgroup
+	if err := m.db.First(&original, id).Error; err != nil {
+		return r, fmt.Errorf("failed to find group: %w", err)
+	}
+
+	if err := original.Archive(m.db, nil); err != nil {
+		return r, fmt.Errorf("failed to unarchive group: %w", err)
+	}
+
+	// Обновляем список
+	r.Emit(
+		presets.NotifModelsUpdated(&models.Bgroup{}),
+		presets.PayloadModelsUpdated{Ids: []string{id}},
+	)
 
 	return r, nil
 }
